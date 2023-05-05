2895d35e025ecc708c01c81784fc15a1f70e3732
Rafael <roboticra@gmail.com>
2023-05-05 15:16:01 -0500

    only cors

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "false");
+  // res.header("Access-Control-Allow-Origin", ); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Credentials", "false");
   //   res.header(
   //     "Access-Control-Allow-Headers",
   //     "Origin, X-Requested-With, Content-Type, Accept"
