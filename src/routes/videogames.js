c4b57127509cfce993448f6ee5afb7d08669e079
Rafael <roboticra@gmail.com>
2023-05-05 18:33:05 -0500

    test fetch api

--- src/routes/videogames.js
+++ src/routes/videogames.js
@@ -17,7 +17,7 @@ async function getVideogames(req, res) {
       res.status(200).json({ api: api, db: db, name });
     } else {
       const db = await fetchVideogameDbbyName(name);
-      // const api = await fetchVideogameApibyName(name);
+      const api = await fetchVideogameApibyName(name);
       res.status(200).json({ api: process.env.API_KEY, db: db, name });
     }
   } catch (error) {
