ede873b65173aebbf4fa591e425657a2535028f0
Rafael <roboticra@gmail.com>
2023-05-05 23:18:48 -0500

    por el momento dev

--- src/app.js
+++ src/app.js
@@ -15,11 +15,11 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", [
-    "http://localhost:3000",
-    "https://gamespiwebapp-production.up.railway.app/",
-  ]); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
+  // res.header("Access-Control-Allow-Origin", [
+  //   "http://localhost:3000",
+  //   "https://gamespiwebapp-production.up.railway.app/",
+  // ]); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
